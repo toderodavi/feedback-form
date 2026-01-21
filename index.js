fa458eb3863bd07975626a48938c5f8fe180cb32
toderodavi <estrela.alem@gmail.com>
2026-01-21 12:51:13 -0300

    feat: textAreaFeedback

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import { labelAsInput } from './js-modules/labelAsInput.js'
 import { moveBtn } from './js-modules/moveBtn.js'
 import { numberSlider } from './js-modules/numberSlider.js'
 import { submitBtn } from './js-modules/submitBtn.js'
-import { textAreaFeedback } from './js-modules/textarea.js'
+import { textAreaFeedback } from './js-modules/textAreaFeedback.js'
 
 apologyOverlay()
 labelAsInput()
