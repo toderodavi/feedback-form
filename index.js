ef9d139936490fd5522599f8c7cb875aeb5dfaf2
toderodavi <estrela.alem@gmail.com>
2026-01-21 09:25:20 -0300

    feat: numberSlider

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import { apologyOverlay } from './js-modules/apologyOverlay.js'
 import { labelAsInput } from './js-modules/labelAsInput.js'
 import { moveBtn } from './js-modules/moveBtn.js'
-import { numberSlider } from './js-modules/slider.js'
+import { numberSlider } from './js-modules/numberSlider.js'
 import { submitBtn } from './js-modules/submitBtn.js'
 import { textAreaFeedback } from './js-modules/textarea.js'
 
